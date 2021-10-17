99c20ab8d55d8eae472fcea583d78e12092d00b5
NicolasSlmetal <nlimadasilvabrito@gmail.com>
2021-10-17 12:25:31 -0300

    Verificação

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -22,6 +22,8 @@ public class Main {
                     .build();
         } catch (LoginException le){
             le.printStackTrace();
+        } catch (NullPointerException e){
+            e.printStackTrace();
         }
     }
 }

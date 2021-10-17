d38c7826b208c201657d3cab0f0e5784e34a4545
NicolasSlmetal <nlimadasilvabrito@gmail.com>
2021-10-17 14:47:33 -0300

    Adição de recurso de criação de arquivo

--- src/main/java/com/play4ubot/utilities/FileManager.java
+++ src/main/java/com/play4ubot/utilities/FileManager.java
@@ -13,7 +13,7 @@ public class FileManager {
     private File dir;
     private String[] symbols;
     public FileManager(){
-        this.dir = new File(System.getProperty("user.dir") + "com.play4ubot\\audiofiles");
+        this.dir = new File(System.getProperty("user.dir"));
         try {
             for (File f : this.dir.listFiles()) {
                 this.musics.add(f.getName().replace("_", " "));
